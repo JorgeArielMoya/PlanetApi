8f93db0312f40a742e80e646bf00bbc905f90bf9
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-18 22:13:23 -0400

    Agrega la implementacion del repositorio de planeta

---
+++ app/src/main/java/edu/ucne/planetapi/data/repository/PlanetRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package edu.ucne.planetapi.data.repository
+
+import edu.ucne.planetapi.data.remote.Resource
+import edu.ucne.planetapi.data.remote.remotedatasource.PlanetRemoteDataSource
+import edu.ucne.planetapi.domain.model.Planet
+import edu.ucne.planetapi.domain.repository.PlanetRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class PlanetRepositoryImpl @Inject constructor(
+    private val remoteDataSource: PlanetRemoteDataSource
+) : PlanetRepository {
+
+    override fun getPlanets(
+        page: Int,
+        limit: Int,
+        name: String?
+    ): Flow<Resource<List<Planet>>> = flow {
+        emit(Resource.Loading())
+        remoteDataSource.getPlanets(page, limit, name)
+            .onSuccess { dto ->
+                emit(Resource.Success(dto.items.map { it.toDomain() }))
+            }.onFailure {
+                emit(Resource.Error(it.message ?: "Error desconocido"))
+            }
+    }
+
+    override fun getPlanetDetail(id: Int): Flow<Resource<Planet>> = flow {
+        emit(Resource.Loading())
+        remoteDataSource.getPlanetDetail(id)
+            .onSuccess { dto ->
+                emit(Resource.Success(dto.toDomain()))
+            }.onFailure {
+                emit(Resource.Error(it.message ?: "Error desconocido"))
+            }
+    }
+}
