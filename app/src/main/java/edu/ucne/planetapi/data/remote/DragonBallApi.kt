ac71f5971436dad215c3329a1dea645e5e235646
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-18 22:11:34 -0400

    Agrega la interfaz de la api de dragon ball para poder comunicarse con ella

---
+++ app/src/main/java/edu/ucne/planetapi/data/remote/DragonBallApi.kt
@@ -0,0 +1,23 @@
+package edu.ucne.planetapi.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import edu.ucne.planetapi.data.remote.dto.PlanetDto
+import edu.ucne.planetapi.data.remote.dto.PlanetResponseDto
+
+interface DragonBallApi {
+
+    @GET("planets")
+    suspend fun getPlanets(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?
+    ): Response<PlanetResponseDto>
+
+    @GET("planets/{id}")
+    suspend fun getPlanetDetail(
+        @Path("id") id: Int
+    ): Response<PlanetDto>
+}
