57272c3035f5eea5e733b05bb562136ca0b9b63f
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-19 20:03:32 -0400

    Agrega la lista de planetas filtrados

--- app/src/main/java/edu/ucne/planetapi/presentation/list/PlanetListUiState.kt
+++ app/src/main/java/edu/ucne/planetapi/presentation/list/PlanetListUiState.kt
@@ -5,6 +5,7 @@ import edu.ucne.planetapi.domain.model.Planet
 data class PlanetListUiState(
     val isLoading: Boolean = false,
     val planets: List<Planet> = emptyList(),
+    val planetsFiltradas: List<Planet> = emptyList(),
     val error: String? = null,
     val filterName: String = ""
 )
