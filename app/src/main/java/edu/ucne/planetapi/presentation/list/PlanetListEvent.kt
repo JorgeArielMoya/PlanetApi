b31c0e1499f1ff805e9264d21543b5788f1f06e0
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-19 20:03:52 -0400

    Elimina el evento search porque no es necesario para este tipo de busqueda sin el boton

--- app/src/main/java/edu/ucne/planetapi/presentation/list/PlanetListEvent.kt
+++ app/src/main/java/edu/ucne/planetapi/presentation/list/PlanetListEvent.kt
@@ -2,5 +2,4 @@ package edu.ucne.planetapi.presentation.list
 
 sealed interface PlanetListEvent {
     data class UpdateName(val name: String) : PlanetListEvent
-    data object Search : PlanetListEvent
 }
