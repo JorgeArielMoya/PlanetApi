846615f91588d4a0596ff438a322e1e237f7acbd
jorge <jorge@LAPTOP-D8GDLQRL>
2026-06-26 18:43:54 -0400

    Agrega el cambio de evento para filtrar por raza y por genero

--- app/src/main/java/edu/ucne/planetapi/presentation/character/list/CharacterListEvent.kt
+++ app/src/main/java/edu/ucne/planetapi/presentation/character/list/CharacterListEvent.kt
@@ -2,4 +2,6 @@ package edu.ucne.planetapi.presentation.character.list
 
 sealed interface CharacterListEvent {
     data class UpdateName(val name: String) : CharacterListEvent
+    data class UpdateRace(val race: String) : CharacterListEvent
+    data class UpdateGender(val gender: String) : CharacterListEvent
 }
